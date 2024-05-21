814c7c5a5d5a8b82b8c32261df92b950332feb30
Berbascum <berbascum@ticv.cat>
2024-05-21 15:30:04 +0200

    Update python prebuilt downloader script from droidian dir

--- droidian/scripts/python-zlib-upgrade.sh
+++ droidian/scripts/python-zlib-upgrade.sh
@@ -3,32 +3,51 @@
 ## Berbascum's script to upgrade the python prebuilt used by the Droidian buils tools to one with zlib support"
 
 ## Install a python prebuilt with zlib module
+
 echo; echo "Checking for python with zlib support..."
-read -p "Pauseta..."
 INI_DIR=$(pwd)
 PYTHON_BASE_VER="2.7"
 PYTHON_SUB_VER="2.7.5"
-PYTHON_PREFIX="/opt/android/prebuilts/python"
+PYTHON_PREFIX="/opt/python"
 HOST_ARCH="i386"
 SUITE="stretch"
-export PATH="${PYTHON_PREFIX}/${PYTHON_SUB_VER}/bin:$PATH"
+
+export PATH="${PYTHON_PREFIX}/${PYTHON_SUB_VER}/bin:${PATH}"
 export LD_LIBRARY_PATH="${PYTHON_PREFIX}/${PYTHON_SUB_VER}/lib:${LD_LIBRARY_PATH}"
 #export PYTHONPATH="/opt/android/prebuilts/python/${PYTHON_SUB_VER}/lib/python${PYTHON_BASE_VER}"
-python2 --version
-python2 -c "import zlib"
-[ -z "$(python2 -c "import zlib" 2>&1 >/dev/null)" ] && PY_ZLIB_FOUND="True" || PY_ZLIB_FOUND="False"
+
+#python22 --version
+#python22 -c "import zlib"
+
+[ -n "$(which python22)" ] && PY_ZLIB_FOUND="True" || PY_ZLIB_FOUND="False"
+
 if [ "${PY_ZLIB_FOUND}" == "False" ]; then
-    echo; echo "zlib python support not availabe. Trying to download a prebuit with zlib support..."
-    PY_PREBUILT_BASE_URL="https://github.com/berbascum/berb-python-prebuilts/raw/main"
-    #PY_PREBUILT_TAR="python${PYTHON_SUB_VER}/python${PYTHON_SUB_VER}-${HOST_ARCH}-${SUITE}-prebuilt_with-zlib-mod.tar.gz"
-    PY_PREBUILT_TAR="python${PYTHON_SUB_VER}/python${PYTHON_SUB_VER}-prebuilt-${SUITE}-${HOST_ARCH}-shared_prefix-droidian-android-toolchain.tar.gz"
-    wget -O /buildd/sources/python${PYTHON_SUB_VER}-${HOST_ARCH}-prebuilt.tar.gz \
-        "${PY_PREBUILT_BASE_URL}/${PY_PREBUILT_TAR}"
-    mv "${PYTHON_PREFIX}/${PYTHON_SUB_VER}" "${PYTHON_PREFIX}/${PYTHON_SUB_VER}_orig"
+    echo; echo "\"zlib\" python support not availabe. Trying to download a prebuit with zlib support..."
+    ## Create python dest path if not exist
+    [ -d "${PYTHON_PREFIX}" ] || mkdir ${PYTHON_PREFIX}
+
+    ## Get the prebuil tar.gz file
+    PY_PREBUILT_BASE_URL="https://github.com/berbascum/berb-python-prebuilts/raw/main/python${PYTHON_SUB_VER}"
+    PY_PREBUILT_TAR="python${PYTHON_SUB_VER}-${HOST_ARCH}-${SUITE}-prebuilt-zlib_shared_prefix-droidian-android-toolchain.tar.gz"
+    #wget -O "/root/${PY_PREBUILT_TAR}" "${PY_PREBUILT_BASE_URL}/${PY_PREBUILT_TAR}"
+    wget "${PY_PREBUILT_BASE_URL}/${PY_PREBUILT_TAR}"
+
+    ## Extract the downloaded python prebuilt
     cd ${PYTHON_PREFIX}
-    tar zxvf /buildd/sources/python${PYTHON_SUB_VER}-${HOST_ARCH}-prebuilt.tar.gz
-    rm -v /buildd/sources/python${PYTHON_SUB_VER}-${HOST_ARCH}-prebuilt.tar.gz
+    #mv "${PYTHON_SUB_VER}" "${PYTHON_SUB_VER}_orig"
+    tar zxvf "${INI_DIR}/${PY_PREBUILT_TAR}"
+    cd ${PYTHON_SUB_VER}/bin
+    mv ./python ./python22
     cd ${INI_DIR}
+    rm -v "${PY_PREBUILT_TAR}"
+
 else
     echo; echo "zlib python module support detected."
 fi
+
+python22 --version
+echo; echo "Final check for zlib:"
+[ -z "$(python22 -c "import zlib" 2>&1 >/dev/null)" ] && PY_ZLIB_FOUND="True" || PY_ZLIB_FOUND="False"
+
+[ "${PY_ZLIB_FOUND}" == "False" ] && echo "zlib support installation failed" && exit
+[ "${PY_ZLIB_FOUND}" == "True" ] && echo "zlib support is installed" && exit
