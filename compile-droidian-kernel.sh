8d29cb74834372d331af3e7f5fd5bcd7a1ae8e4b
Berbascum <berbascum@ticv.cat>
2024-05-23 03:00:05 +0200

    Update mkdtboimg.py script with the droidian packaged one
    Update python2 to python3 on the Makefile.lib mkdtboimg command definition

--- compile-droidian-kernel.sh
+++ compile-droidian-kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export PATH=/bin:/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#export PATH=/bin:/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 chmod +x /buildd/sources/debian/rules
 cd /buildd/sources
 rm -f debian/control
 debian/rules debian/control
-source /buildd/sources/droidian/scripts/python-zlib-upgrade.sh
+#source /buildd/sources/droidian/scripts/python-zlib-upgrade.sh
 
 RELENG_HOST_ARCH="arm64" releng-build-package
